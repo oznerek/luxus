4e55cd97d930682875e9354a8ac4a96560e63ba9
oznerek <oznerek@gmail.com>
2019-08-15 19:19:29 +0200

    Updates

---
+++ precache-manifest.491e2e613ef02dfdfc11947ccee38f73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff1d5d8a947bde1115f9db38355665c",
+    "url": "/luxus/index.html"
+  },
+  {
+    "revision": "8edb9c45c61842379209",
+    "url": "/luxus/static/js/2.1a0b98c6.chunk.js"
+  },
+  {
+    "revision": "32b3e0fb4a73ffb31e9a",
+    "url": "/luxus/static/js/main.f28c1b16.chunk.js"
+  },
+  {
+    "revision": "2b9b39d658529e18897f",
+    "url": "/luxus/static/js/runtime~main.537069ab.js"
+  }
+]);
